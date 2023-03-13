0710339a064c5f01c59b5b0a73a6370348fb0507
Gilles Takam Cheno <chenogilles@gmail.com>
2023-03-13 16:23:56 +0100

    change test_valid_flight_page function in flights/tests.py file

--- flights/tests.py
+++ flights/tests.py
@@ -57,8 +57,8 @@ class FlightTestCase(TestCase):
     def test_invalid_flight_page(self):
         max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
         c = Client()
-        response = c.get(f"/flights/{max_id + 1}")
-        self.assertEqual(response.status_code, 404)
+        # response = c.get(f"/flights/{max_id + 1}")
+        self.assertEqual(c.get(f"/flights/{max_id + 1}").status_code, 404)
 
     def test_flight_page_passengers(self):
         f = Flight.objects.get(pk=1)
